520ed79bc02a7388119ebc583ff73b54ae870b1c
Santhosh-1997 <99santhoshs@gmail.com>
2022-02-22 18:18:22 +0530

    homepage except footer and js-- done

---
+++ components/navbar.js
@@ -0,0 +1,42 @@
+function navbar(){
+    return `
+    <div id="leftheader">
+    <div id="options">
+        <i class="fa-solid fa-bars fa-2x"></i>    
+    </div>
+    <div id="search">
+        <i class="fa-solid fa-magnifying-glass fa-2x"></i>
+        <input id="searchbar" type="search">
+    </div>
+    
+</div>
+<div id="logo">
+    <img src="https://www.stpaulcollection.com/wp-content/uploads/2018/11/st-paul-collection-amenities-cb2.png" alt="">
+</div>
+<div id="rightheader">
+    <ul>
+        <li>
+            <div>
+                <i class="fa-solid fa-location-dot fa-2x"></i>
+            </div>
+        </li>
+        <li>
+            <div>
+                <i class="fa-solid fa-user fa-2x"></i>
+            </div>
+        </li>
+        <li>
+            <div>
+                <i class="fa-solid fa-heart fa-2x"></i>
+            </div>
+        </li>
+        <li>
+            <div>
+                <i class="fa-solid fa-cart-shopping fa-2x"></i>
+            </div>
+        </li>
+    </ul>
+</div>`
+}
+
+export default navbar
